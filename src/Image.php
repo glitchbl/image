bfc9f1c9ee798dc6982b11415c2b819d3e034207
Créaforcom <quentin@creaforcom.fr>
2022-03-01 16:58:16 +0100

    clone() + crop()  + save()

--- src/Image.php
+++ src/Image.php
@@ -13,17 +13,17 @@ class Image
     /**
      * @var mixed Image resource
      */
-	protected $image_resource = null;
+	protected $resource = null;
 
     /**
      * @var int Image width
      */
-	protected $image_width;
+	protected $width;
 
     /**
      * @var int Image height
      */
-	protected $image_height;
+	protected $height;
 
     /**
      * @var string Image extension
@@ -47,60 +47,54 @@ class Image
 
 	    switch ($image_type) {
 	        case IMAGETYPE_GIF:
-	            $this->image_resource = imagecreatefromgif($file);
+	            $this->resource = imagecreatefromgif($file);
 	            $this->extension = self::GIF;
 	            break;
 	        case IMAGETYPE_JPEG:
-	            $this->image_resource = imagecreatefromjpeg($file);
+	            $this->resource = imagecreatefromjpeg($file);
 	            $this->extension = self::JPG;
 	            break;
 	        case IMAGETYPE_PNG:
-	            $this->image_resource = imagecreatefrompng($file);
+	            $this->resource = imagecreatefrompng($file);
 	            $this->extension = self::PNG;
+				imageAlphaBlending($this->resource, true);
+				imageSaveAlpha($this->resource, true);
 	            break;
 		}
 		
-        $exif = @exif_read_data($file); // TEMP FIX
+        $exif = @exif_read_data($file);
         $orientation = isset($exif['Orientation'])? $exif['Orientation']: 0;
 
         switch ($orientation) {
             case 2:
-                imageflip($this->image_resource, IMG_FLIP_HORIZONTAL);
+                imageflip($this->resource, IMG_FLIP_HORIZONTAL);
                 break;
             case 3:
-                $this->image_resource = imagerotate($this->image_resource, 180, 0);
+                $this->resource = imagerotate($this->resource, 180, 0);
                 break;
             case 4:
-                imageflip($this->image_resource, IMG_FLIP_VERTICAL);
+                imageflip($this->resource, IMG_FLIP_VERTICAL);
                 break;
             case 5:
-                $this->image_resource = imagerotate($this->image_resource, -90, 0);
-                imageflip($this->image_resource, IMG_FLIP_HORIZONTAL);
+                $this->resource = imagerotate($this->resource, -90, 0);
+                imageflip($this->resource, IMG_FLIP_HORIZONTAL);
                 break;
             case 6:
-                $this->image_resource = imagerotate($this->image_resource, -90, 0);
+                $this->resource = imagerotate($this->resource, -90, 0);
                 break;
             case 7:
-                $this->image_resource = imagerotate($this->image_resource, 90, 0);
-                imageflip($this->image_resource, IMG_FLIP_HORIZONTAL);
+                $this->resource = imagerotate($this->resource, 90, 0);
+                imageflip($this->resource, IMG_FLIP_HORIZONTAL);
                 break;
             case 8:
-                $this->image_resource = imagerotate($this->image_resource, 90, 0); 
+                $this->resource = imagerotate($this->resource, 90, 0); 
                 break;
         }
 
-	    $this->image_width = imagesx($this->image_resource);
-	    $this->image_height = imagesy($this->image_resource);
+	    $this->width = imagesx($this->resource);
+	    $this->height = imagesy($this->resource);
 
 	    $this->file = $file;
-	}
-
-    /**
-     * @return string Image extension
-     */
-	public function getExtension()
-	{
-		return $this->extension;
 	}
 
 	/**
@@ -113,152 +107,265 @@ class Image
 		return $this->{$name};
 	}
 
-    /**
-     * @param int $max_width Max resized image width
-     * @param int $max_height Max resized image height
-	 * @return mixed Resource
-     */
-	protected function _resize($max_width = 1000, $max_height = 1000)
+	/**
+	 * @return mixed Image
+	 */
+	function clone()
 	{
-	    $destination_width = $this->image_width;
-	    $destination_height = $this->image_height;
+		$width = imagesx($this->resource);
+		$height = imagesy($this->resource);
+		$transparancy = imagecolortransparent($this->resource);
 
-	    if ($this->image_width / $this->image_height >= 1) {
-	        if ($this->image_width > $max_width) {
-	            $ratio = $max_width / $this->image_width;
-	            $destination_width = $max_width;
-	            $destination_height = (int)($this->image_height * $ratio);
-	        }
-            if ($destination_height > $max_height) {
-	            $ratio = $max_height / $destination_height;
-	            $destination_height = $max_height;
-	            $destination_width = (int)($destination_width * $ratio);
-            }
-	    } else {
-	        if ($this->image_height > $max_height) {
-	            $ratio = $max_height / $this->image_height;
-	            $destination_height = $max_height;
-	            $destination_width = (int)($this->image_width * $ratio);
-	        }
-            if ($destination_width > $max_width) {
-	            $ratio = $max_width / $destination_width;
-	            $destination_width = $max_width;
-	            $destination_height = (int)($destination_height * $ratio);
-            }
-	    }
+		if (imageistruecolor($this->resource)) {
+			$clone = imagecreatetruecolor($width, $height);
+			imagealphablending($clone, false);
+			imagesavealpha($clone, true);
+		} else {
+			$clone = imagecreate($width, $height);
 
-	    $destination_resource = imagecreatetruecolor($destination_width, $destination_height);
-	    $white = imagecolorallocate($destination_resource, 255, 255, 255);
-	    imagefill($destination_resource, 0, 0, $white);
-	    imagecopyresampled($destination_resource, $this->image_resource, 0, 0, 0, 0, $destination_width, $destination_height, $this->image_width, $this->image_height);
+			if ($transparancy >= 0) {
+				$rgb = imagecolorsforindex($this->resource, $transparancy);
+				imagesavealpha($clone, true);
+				$transparancyIndex = imagecolorallocatealpha($clone, $rgb['red'], $rgb['green'], $rgb['blue'], $rgb['alpha']);
+				imagefill($clone, 0, 0, $transparancyIndex);
+			}
+		}
 
-		return $destination_resource;
+		imagecopy($clone, $this->resource, 0, 0, 0, 0, $width, $height);
+
+		return $clone;
 	}
 
-    /**
-     * @param string $destination Resized image location
-     * @param string $extension Image extention (PNG or JPG)
-     * @param int $max_width Max resized image width
-     * @param int $max_height Max resized image height
-     */
-	public function resize($destination, $extension = self::PNG, $max_width = 1000, $max_height = 1000)
-	{
-		$destination_resource = $this->_resize($max_width, $max_height);
-        $this->save($destination_resource, $extension, $destination);
-	    imagedestroy($destination_resource);
-	}
-
-    /**
-     * @param int $max_width Max resized image width
-     * @param int $max_height Max resized image height
-     */
-	public function resizeSelf($max_width = 1000, $max_height = 1000)
-	{
-		$resource = $this->_resize($max_width, $max_height);
-		$width = imagesx($resource);
-		$height = imagesy($resource);
-		imagedestroy($this->image_resource);
-		$this->image_resource = $resource;
-		$this->image_width = $width;
-		$this->image_height = $height;
-	}
-
-    /**
-     * @param int $type Filter type
-     * @param void
-     */
+	/**
+	 * @param int $type Filter type
+	 * @param void
+	 */
 	public function filter($type)
 	{
-		imagefilter($this->image_resource, $type);
+		imagefilter($this->resource, $type);
+	}
+
+	/**
+	 * @param string $fontPath
+	 * @param string $text
+	 * @param int $x
+	 * @param int $y
+	 * @param array $align
+	 * @param int $fontSize
+	 * @param array $textColor
+	 * @param array $bgColor
+	 * @param int $padding
+	 */
+	public function addText($fontPath, $text, $x = 0, $y = 0, $align = ['top', 'left'], $fontSize = 32, $textColor = [255, 255, 255], $bgColor = [255, 255, 255, 127], $padding = 0)
+	{
+		if (!is_file($fontPath))
+			throw new Exception("{$fontPath} does not exist");
+
+		$bbox = imagettfbbox($fontSize, 0, $fontPath, $text);
+
+		$imageText = imagecreatetruecolor($this->width, 0 - $bbox[5] + $bbox[1] + $padding * 2);
+
+		$imageBgColor = imagecolorallocatealpha($imageText, $bgColor[0], $bgColor[1], $bgColor[2], $bgColor[3]);
+		imagefill($imageText, 0, 0, $imageBgColor);
+
+		if ($align[1] == 'center') {
+			$newX = round(($this->width - $bbox[2]) / 2);
+		} elseif ($align[1] == 'right') {
+			$newX = $this->width - $x;
+		} else {
+			$newX = $x;
+		}
+
+		imagettftext($imageText, $fontSize, 0, $newX, 0 - $bbox[5] + $padding, imagecolorallocate($imageText, $textColor[0], $textColor[1], $textColor[2]), $fontPath, $text);
+
+		$this->addImage($imageText, 0, $y, $align);
+		imagedestroy($imageText);
+	}
+
+	/**
+	 * @param static $image
+	 * @param int $x
+	 * @param int $y
+	 * @param array $align
+	 */
+	public function addImage($image, $x = 0, $y = 0, $align = ['top', 'left'])
+	{
+		if ($align[1] == 'center') {
+			$newX = round(($this->width - $image->width) / 2);
+		} elseif ($align[1] == 'right') {
+			$newX = $this->width - $x;
+		} else {
+			$newX = $x;
+		}
+
+		if ($align[0] == 'center') {
+			$newY = round(($this->height - $image->height) / 2);
+		} elseif ($align[0] == 'bottom') {
+			$newY = $this->height - $image->height - $y;
+		} else {
+			$newY = $y;
+		}
+
+		$newImageResource = imagecreatetruecolor($this->width, $this->height);
+		$transparentColor = imagecolorallocatealpha($newImageResource, 0, 0, 0, 127);
+		imagefill($newImageResource, 0, 0, $transparentColor);
+
+		imagecopy($newImageResource, $this->resource, 0, 0, 0, 0, $this->width, $this->height);
+
+		imagecopy($newImageResource, $image->resource, $newX, $newY, 0, 0, $image->width, $image->height);
+
+		imagedestroy($this->resource);
+		$this->resource = $newImageResource;
 	}
 
     /**
-     * @param string $destination Thumb location
-     * @param int $thumb_width Thumb width
-     * @param int $thumb_height Thumb height
-     * @param string $extension Image extention (PNG or JPG)
-     * @param int $offset_x Thumb X Offset
-     * @param int $offset_x Thumb Y Offset
+     * @param int $maxWidth Max resized image width
+     * @param int $maxHeight Max resized image height
 	 * @return void
      */
-	public function thumb($destination, $thumb_width, $thumb_height, $extension = self::PNG, $offset_x = 0, $offset_y = 0)
+	protected function resize($maxWidth = 1000, $maxHeight = 1000)
 	{
-	    $cut_width = $this->image_width;
-	    $cut_height = $this->image_height;
+	    $dstWidth = $this->width;
+	    $dstHeight = $this->height;
 
-		$ratio_height_to_width = $thumb_width / $thumb_height;	    
-		$ratio_width_to_height = $thumb_height / $thumb_width;	  
-
-	    if ($cut_width <= $cut_height)
-	    	$cut_height = (int)($cut_width * $ratio_width_to_height);
-	    else
-	    	$cut_width = (int)($cut_height * $ratio_height_to_width);
-
-	    if ($cut_width > $this->image_width) {
-	    	$ratio = $this->image_width / $cut_width;
-	    	$cut_width = $this->image_width;
-	    	$cut_height = (int)($ratio * $cut_height);
+	    if ($this->width / $this->height >= 1) {
+	        if ($this->width > $maxWidth) {
+	            $ratio = $maxWidth / $this->width;
+	            $dstWidth = $maxWidth;
+	            $dstHeight = (int)($this->height * $ratio);
+	        }
+            if ($dstHeight > $maxHeight) {
+	            $ratio = $maxHeight / $dstHeight;
+	            $dstHeight = $maxHeight;
+	            $dstWidth = (int)($dstWidth * $ratio);
+            }
+	    } else {
+	        if ($this->height > $maxHeight) {
+	            $ratio = $maxHeight / $this->height;
+	            $dstHeight = $maxHeight;
+	            $dstWidth = (int)($this->width * $ratio);
+	        }
+            if ($dstWidth > $maxWidth) {
+	            $ratio = $maxWidth / $dstWidth;
+	            $dstWidth = $maxWidth;
+	            $dstHeight = (int)($dstHeight * $ratio);
+            }
 	    }
 
-	    if ($cut_height > $this->image_height) {
-	    	$ratio = $this->image_height / $cut_height;
-	    	$cut_height = $this->image_height;
-	    	$cut_width = (int)($ratio * $cut_width);
-	    }
+	    $dstResource = imagecreatetruecolor($dstWidth, $dstHeight);
+	    $white = imagecolorallocate($dstResource, 255, 255, 255);
+	    imagefill($dstResource, 0, 0, $white);
+	    imagecopyresampled($dstResource, $this->resource, 0, 0, 0, 0, $dstWidth, $dstHeight, $this->width, $this->height);
 
-	    $start_x = round(($this->image_width / 2) - ($cut_width / 2) + $offset_x);
-            if ($start_x < 0)
-                 $start_x = 0;
-            elseif ($start_x + $cut_width > $this->image_width)
-                 $start_x = $this->image_width - $cut_width;
-
-	    $start_y = round(($this->image_height / 2) - ($cut_height / 2) + $offset_y);
-            if ($start_y < 0)
-                $start_y = 0;
-            elseif ($start_y + $cut_height > $this->image_height)
-                $start_y = $this->image_height - $cut_height;
-
-	    $destination_resource = imagecreatetruecolor($thumb_width, $thumb_height);
-	    $white = imagecolorallocate($destination_resource, 255, 255, 255);
-	    imagefill($destination_resource, 0, 0, $white);
-	    imagecopyresampled($destination_resource, $this->image_resource, 0, 0, $start_x, $start_y, $thumb_width, $thumb_height, $cut_width, $cut_height);
-
-        $this->save($destination_resource, $extension, $destination);
-		
-	    imagedestroy($destination_resource);
+		imagedestroy($this->resource);
+		$this->resource = $dstResource;
+		$this->width = $dstWidth;
+		$this->height = $dstHeight;
 	}
 
-    /**
-     * @param mixed $resource Image resource
-     * @param string $extension Image extention (PNG or JPG)
-     * @param string $destination Image location
-     */
-    protected function save($resource, $extension, $destination)
-    {
-		if ($extension == 'png') {
-	    	imagepng($resource, $destination, 6);
-		} else {
-			imagejpeg($resource, $destination);
+	/**
+	 * @param int $width Crop width
+	 * @param int $height Crop height
+	 * @param int $x Crop start X
+	 * @param int $y Crop start Y
+	 * @return void
+	 */
+	public function crop($width, $height, $x = 0, $y = 0)
+	{
+		if ($width > $this->width)
+			$width = $this->width;
+
+		if ($height > $this->height)
+			$height = $this->height;
+
+		if ($x < 0)
+			$x = 0;
+		elseif ($width + $x > $this->width)
+			$x = $this->width - $width;
+
+		if ($y < 0)
+			$y = 0;
+		elseif ($y + $height > $this->height)
+			$y = $this->height - $height;
+
+		$dstResource = imagecreatetruecolor($width, $height);
+		$white = imagecolorallocate($dstResource, 255, 255, 255);
+		imagefill($dstResource, 0, 0, $white);
+		imagecopy($dstResource, $this->resource, 0, 0, $x, $y, $width, $height);
+
+		imagedestroy($this->resource);
+		$this->resource = $dstResource;
+		$this->width = $width;
+		$this->height = $height;
+	}
+	/**
+	 * @param string $destination Thumb location
+	 * @param int $thumbWidth Thumb width
+	 * @param int $thumbHeight Thumb height
+	 * @param int $offsetX Thumb X Offset
+	 * @param int $offsetY Thumb Y Offset
+	 * @return void
+	 */
+	public function thumb($thumbWidth, $thumbHeight, $offsetX = 0, $offsetY = 0)
+	{
+		$cutWidth = $this->width;
+		$cutHeight = $this->height;
+
+		$ratioHeightToWidth = $thumbWidth / $thumbHeight;
+		$ratioWidthToHeight = $thumbHeight / $thumbWidth;
+
+		if ($cutWidth <= $cutHeight)
+			$cutHeight = (int)($cutWidth * $ratioWidthToHeight);
+		else
+			$cutWidth = (int)($cutHeight * $ratioHeightToWidth);
+
+		if ($cutWidth > $this->width) {
+			$ratio = $this->width / $cutWidth;
+			$cutWidth = $this->width;
+			$cutHeight = (int)($ratio * $cutHeight);
 		}
-    }
+
+		if ($cutHeight > $this->height) {
+			$ratio = $this->height / $cutHeight;
+			$cutHeight = $this->height;
+			$cutWidth = (int)($ratio * $cutWidth);
+		}
+
+		$startX = round((($this->width - $cutWidth) / 2) + $offsetX);
+		if ($startX < 0)
+			$startX = 0;
+		elseif ($startX + $cutWidth > $this->width)
+			$startX = $this->width - $cutWidth;
+
+		$startY = round((($this->height - $cutHeight) / 2) + $offsetY);
+		if ($startY < 0)
+			$startY = 0;
+		elseif ($startY + $cutHeight > $this->height)
+			$startY = $this->height - $cutHeight;
+
+		$dstResource = imagecreatetruecolor($thumbWidth, $thumbHeight);
+		$white = imagecolorallocate($dstResource, 255, 255, 255);
+		imagefill($dstResource, 0, 0, $white);
+		imagecopyresampled($dstResource, $this->resource, 0, 0, $startX, $startY, $thumbWidth, $thumbHeight, $cutWidth, $cutHeight);
+
+		imagedestroy($this->resource);
+		$this->resource = $dstResource;
+		$this->width = $thumbWidth;
+		$this->height = $thumbHeight;
+	}
+
+	/**
+	 * @param string|null $destination Image location
+	 * @param string|null $extension Image extention (PNG or JPG)
+	 */
+	public function save($destination = null, $extension = null)
+	{
+		$extension = $extension ?? $this->extension;
+
+		if ($extension == static::PNG) {
+			imagepng($this->resource, $destination ?? $this->file, 6);
+		} else {
+			imagejpeg($this->resource, $destination ?? $this->file);
+		}
+	}
 }
